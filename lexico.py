9387a3bed372a37848efa372fccdff79e0dc0ad0
emma <223729@ids.upchiapas.edu.mx>
2025-03-12 18:47:24 -0600

    sintactio escribir

--- lexico.py
+++ lexico.py
@@ -1,19 +1,19 @@
 import ply.lex as lex
 
-# Definición de los tokens
-tokens_principales = [
-    'ENTERO',
+# Lista de tokens
+tokens = [
+    'ENTERO',  
     'FLOTANTE', 
     'VARIABLE',
-    'OPERADOR', 
-    'ASIGNACION', 
+    'OPERADOR',  
+    'ASIGNACION',
     'COMPARACION', 
-    'SEPARADOR',
-    'DELIMITADOR', 
+    'SEPARADOR',  
+    'DELIMITADOR_L',  
+    'DELIMITADOR_R',  
     'COMILLAS', 
-    'DOS_PUNTOS', 
-    'PUNTO',
-    'LOGICO'
+    'LOGICO',
+    'DOS_PUNTOS'    
 ]
 
 # Palabras reservadas
@@ -26,161 +26,77 @@ palabras_reservadas = {
     'leer': 'LEER',
     'val': 'VAL',
     'init': 'INIT',
+    'func': 'FUNC'
 }
 
-tokens = list(palabras_reservadas.values()) + tokens_principales
+# Agregar palabras reservadas a tokens
+tokens += list(palabras_reservadas.values())
 
-# Ignorar los espacios
-t_ignore = ' \t,'
+# Ignorar espacios y tabulaciones
+t_ignore = ' \t'
 
-# Definición de las expresiones regulares 
+# Definición de tokens simples
 t_OPERADOR = r'\+|\-|\*|\/'
+t_COMPARACION = r'<=|>=|==|!=|<|>'
 t_ASIGNACION = r'='
-t_COMPARACION = r'>|<|>=|<=|==|!='
 t_SEPARADOR = r';'
-t_DELIMITADOR = r'\(|\)|\{|\}'  
-t_DOS_PUNTOS = r'\:'
-t_PUNTO = r'\.'
+t_DELIMITADOR_L = r'\{'
+t_DELIMITADOR_R = r'\}'
 t_LOGICO = r'&&|\|\||!'
+t_DOS_PUNTOS = r'\:'
 
-
-# Números decimales
+# Definición de tokens con funciones
 def t_FLOTANTE(t):
     r'\d+\.\d+' 
     t.value = float(t.value)
     return t
 
-# Números enteros
 def t_ENTERO(t):
     r'\d+' 
     t.value = int(t.value)
     return t
 
-
-
 def t_VARIABLE(t):
-    r'[a-zA-Z_][a-zA-Z_]*'
-    t.type = palabras_reservadas.get(t.value, 'VARIABLE')
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = palabras_reservadas.get(t.value, 'VARIABLE')  # Prioriza palabras reservadas
     return t
 
 def t_COMILLAS(t):
     r'\"(.*?)\"'
-    t.value = t.value[1:-1]
+    t.value = t.value[1:-1]  
     return t
 
 def t_COMENTARIOS(t):
-    r'/\*.*?\*/'
+    r'/\*.*?\*/|\/\/.*'
     pass
 
-
-def t_new_line(t):
-    r'\n+' 
+def t_newline(t):
+    r'\n+'
     t.lexer.lineno += len(t.value)
 
-
 def t_error(t):
-    errores.append(f"Caracter ilegal '{t.value[0]}' en la línea {t.lexer.lineno}")
+    print(f"Caracter ilegal '{t.value[0]}' en la línea {t.lexer.lineno}")
     t.lexer.skip(1)
 
+# Construcción del analizador léxico
+analizador = lex.lex()
+
+
+# Función para realizar el análisis léxico
 def analisis(input_code):
     global errores
     errores = []
-    
-  
     analizador = lex.lex()
     analizador.input(input_code)
-    
     tokens_generados = []
     palabras_reservadas_detectadas = []
-    pila = []  
-    esperando_igual = False  
-    esperando_val_variable = False  
-    esperando_igual_val = False  
-    esperando_val_numero = False  
-    esperando_fun_nombre = False  
-    esperando_llave_fun = False  
-    esperando_init = True  
-    en_condicion = False  
-    
     while True:
         tok = analizador.token()
         if not tok:
             break
-        
         tokens_str = f"LexToken({tok.type}, '{tok.value}', {tok.lineno}, {tok.lexpos})"
         if tok.type in palabras_reservadas.values():
             palabras_reservadas_detectadas.append(tokens_str)
         else:
             tokens_generados.append(tokens_str)
-        
-       
-        if tok.type == 'DELIMITADOR':
-            if tok.value in '{(':
-                pila.append(tok.value)
-            elif tok.value in '})':
-                if not pila:
-                    errores.append(f"Error: Se encontró '{tok.value}' sin un delimitador de apertura en la línea {tok.lineno}")
-                else:
-                    top = pila.pop()
-                    if (top == '(' and tok.value != ')') or (top == '{' and tok.value != '}'):
-                        errores.append(f"Error: Delimitador desbalanceado '{top}' en la línea {tok.lineno}")
-        
-     
-        if tok.type == 'INIT' and esperando_init:
-            esperando_init = False  
-        
-      
-        if tok.type == 'FUN':
-            esperando_fun_nombre = True  
-        elif esperando_fun_nombre:
-            if tok.type == 'VARIABLE':
-                esperando_fun_nombre = False
-                esperando_llave_fun = True  
-            else:
-                errores.append(f"Error: Se esperaba un identificador después de 'fun' en la línea {tok.lineno}")
-                esperando_fun_nombre = False
-        elif esperando_llave_fun:
-            if tok.type == 'DELIMITADOR' and tok.value == '{':
-                esperando_llave_fun = False  
-            else:
-                errores.append(f"Error: Se esperaba {{ después de la declaración de función en la línea {tok.lineno}")
-                esperando_llave_fun = False
-
-       
-        if tok.type == 'VAL' and not esperando_igual_val:
-            esperando_val_variable = True  
-        elif esperando_val_variable:
-            if tok.type == 'VARIABLE':
-                esperando_val_variable = False
-                esperando_igual_val = True  
-            else:
-                errores.append(f"Error: Después de 'val' debe ir una variable en la línea {tok.lineno}")
-                esperando_val_variable = False
-        elif esperando_igual_val:
-            if tok.type == 'ASIGNACION':
-                esperando_igual_val = False
-                esperando_val_numero = True
-            else:
-                errores.append(f"Error: Después de la variable debe ir un operador '=' en la línea {tok.lineno}")
-                esperando_igual_val = False
-        elif esperando_val_numero:
-            if tok.type in ['ENTERO', 'FLOTANTE']:
-                esperando_val_numero = False  
-            else:
-                errores.append(f"Error: Después de '=' debe ir un número o valor en la línea {tok.lineno}")
-                esperando_val_numero = False
-
-        
-        if tok.type == 'IF' or tok.type == 'WHILE' or tok.type == 'FOR': 
-            en_condicion = True
-        elif en_condicion and tok.type == 'DELIMITADOR' and tok.value == ')':  
-            en_condicion = False
-
- 
-    if esperando_init:
-        errores.append("Error: Se debe definir la función 'init' para iniciar el programa.")
-    
-    if pila:
-        errores.append("Error: Hay delimitadores de apertura sin cerrar: " + "".join(pila))
-    
     return tokens_generados, palabras_reservadas_detectadas, errores
